a1fe77b56f2faaa5c91a8a5231ff17c5339e8868
Satar07 <69021280+Satar07@users.noreply.github.com>
2020-10-29 17:14:58 +0800

    Update 1.12.7.cpp
    船新版本  ac

--- 1.12.7.cpp
+++ 1.12.7.cpp
@@ -1,33 +1,40 @@
 #include<iostream>
-#include<cstdio>
 using namespace std;
 
-int a[2000],ro[100000];   //room
+int room[200],dict[1010];
+int M,N,look;
 
+bool if_found(int word){             //查找是否已经出现在内存中
+	if(dict[word]==1)return true;
+	return false;
+}
+
+int turn(int Fword,int Bword){       //检测是否内存已经满了；满了就换，没有就在dictionary里面对应位置改ture
+	if(look<=M-1){
+		dict[Bword]=1;
+	}
+	else if(look>=M){
+		dict[Fword]=0;
+		dict[Bword]=1;
+	}
+	look++;
+	return 1;
+	
+}
 
 int main(){
-	int m,n,time=1,cc=0,first=1;
-	cin>>m>>n;
-	int temp;
-	for(int i=1;i<=n;i++){
-		cin>>temp;
-		
-		if(time==m+1){
-			first=0;           //内存满了，以后不重复的都要删去前面一个 
-			time=1;
+	cin>>M>>N;
+	int j=1,now,temp;
+	for(int i=1;i<=N;i++){
+		j=(j==M+1)?1:j;     //j的值在1到M里面循环
+		cin>>now;
+		if (if_found(now)) continue;
+		else{
+			temp=room[j];
+			room[j]=now;
+			turn(temp,room[j]);
+			j++;        //注意语句在循环体里面
 		}
-		
-		if(first==0){
-			a[ro[time]]=0;     //删除内存中的已经查找过的记录 
-		}
-		
-		if(a[temp]==0){        //判断没有出现过 
-			a[temp]=1;
-			ro[time]=temp;     //模拟内存 
-			cc++;
-			time++;
-		}
-		
 	}
-	cout<<cc;
+	cout<<look;
 }
